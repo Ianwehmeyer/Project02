95d6e17ca156f406482374c3f45de0ddb4f2231d
Ianwehmeyer <65263828+Ianwehmeyer@users.noreply.github.com>
2020-07-12 19:16:57 -0400

    console.log (url)
    I think I can call the url (not sure the right word for that,) and use it as a value.

--- giphy.js
+++ giphy.js
@@ -6,7 +6,11 @@ $(document).ready(function(){
             //supposedly this will store the text from search id under query
 
         let query = $('#search').val();
-        let url
+        let url = 'http://api.giphy.com/v1/gifs/search?q='+query+'&api_key=dc6zaTOxFJmzC';
+
+        //I think console.log might be necessary next. When tested I do show the URL in the console log!!!
+
+        console.log(url);
 
 
     });
