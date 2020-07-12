f354fd85a65f2e0b1ed06b355a13fe72f85ee011
Ianwehmeyer <65263828+Ianwehmeyer@users.noreply.github.com>
2020-07-12 19:10:57 -0400

    Let function
    I think I can use the let function to store the text in the box under 'query'

--- giphy.js
+++ giphy.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
     //I think I have to reference the button first
-    $("#submit").on('click', function() {
-        
+    $("#submit").on('click', function(e) {
+        e.preventDefault();
+
+            //supposedly this will store the text from search id under query
+
+        let query = $('#search').val();
+        let url
 
 
     });
